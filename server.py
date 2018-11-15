a38c2657ca708bb25d0c5f4d7e23e29881ba3f2f
Oleg <super.oleg.3000@yandex.ru>
2018-11-15 15:05:56 +0300

    Add article post method stub

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def form():
     return render_template('form.html')
+
+@app.route('/api/articles', methods=['POST'])
+def post_article():
+    data = request.json
+    print(data.get('header'), data.get('signature'), data.get('body'))
+    return "Ok"
 
 if __name__ == "__main__":
     app.run(debug=True)
