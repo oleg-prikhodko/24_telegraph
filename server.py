54ef9239cf0d0dd1dc701cc485e255706864a5b2
Oleg <super.oleg.3000@yandex.ru>
2018-11-16 13:17:47 +0300

    Put userid into session

--- server.py
+++ server.py
@@ -1,20 +1,37 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, session
+from uuid import uuid1
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@app.route('/')
+
+@app.route("/")
 def form():
-    return render_template('form.html')
+    if "userid" not in session:
+        session["userid"] = uuid1().hex
 
-@app.route('/api/articles', methods=['POST'])
+    return render_template("form.html")
+
+
+@app.route("/api/articles", methods=["POST"])
 def post_article():
-    data = request.json
-    print(data.get('header'), data.get('signature'), data.get('body'))
-    return "Ok"
+    response = make_response("OK")
 
-@app.route('api/articles', methods=['PUT'])
+    data = request.json
+    print(data.get("header"), data.get("signature"), data.get("body"))
+
+    return response
+
+
+@app.route("/sessiontest")
+def session_test():
+    return "userid: {}".format(session.get("userid"))
+
+
+@app.route("/api/articles", methods=["PUT"])
 def update_article():
     pass
+
 
 if __name__ == "__main__":
     app.run(debug=True)
