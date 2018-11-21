8841d0c6f79255f145a4a3f4f468a3b9dca9154f
Oleg <super.oleg.3000@yandex.ru>
2018-11-22 01:13:46 +0300

    Update readme

--- server.py
+++ server.py
@@ -94,5 +94,5 @@ def update_article():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 33507))
+    port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
